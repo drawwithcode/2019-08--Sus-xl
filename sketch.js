798ad28e943b8ae532c8af5f00a11b5b0168b12e
xushan <xushan.xushan@gmail.com>
2019-12-30 17:11:50 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
-function preload(){
-  // put preload code here
-}
-
+// must be in HTTPS
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  console.log('starting');
+	noStroke();
+  // get position once
+  if (!navigator.geolocation) {
+    alert("navigator.geolocation is not available");
+  }
+  navigator.geolocation.getCurrentPosition(setPos);
 }
 
-function draw() {
-  // put drawing code here
+function setPos(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  background(0);
+  fill(255);
+  textSize(32);
+  text("Current position: " + nf(lat,2,2) + " " + nf(lng,2,2), 10, height/2);
 }
